0651a22bed40fdf3628da2aa29c4b97c9c5d9c6d
James Mancuso <43454765+Jmancuso1999@users.noreply.github.com>
2021-02-22 14:21:12 -0500

    Update activity.py

--- activity.py
+++ activity.py
@@ -63,31 +63,3 @@ if __name__ == "__main__":
     spacs = mostTweeted(symbols, api)
     top3 = mostDiscussed(spacs)
     sendTweet(top3, api)
-    
-"""
-
-- Scrape all tweets of the symbols (for loop of the range of the symbols)
-- Put the amount od times the symbol appears in the dictonary into
-- https://docs.tweepy.org/en/latest/api.html
-
-
-- Then do the stuff below:
-
-    (Need to make it so the script always runs as well)
-        - a CRON script will allow us to run time at a set time
-                - NOTE: This is a macbook SCHEDULER --> so clone this git repo (when uploaded and run this on my MAC) --> This should work for tweeting out scheduled outputs 
-
-                - NOTE: CRON ONLY!!!! runs WHEN MAC IS ON --> ANACRON can schedule scripts when mac is shut down
-
-    1. Figure out how to see if one of the SPAC symbols is trending (maybe find a way to fetch like every 5 mins)
-            - Reads the trending 
-    2. Then figure out how to write the trends to a twitter account to tweet
-
-
-    Batch processing --> (Apache Spark) --> NOT processed in real time, meaning that it can take like a day to process the data
-    Real Time Processing --> (Apache Kafka) --> Processes the data the day of and is used for real time analytics
-
-    Airflow --> Purpose is to schedule jobs --> Directed Acyclic Graphs or DAGs are different jobs that are scheduled (might not be fully correct)
-            - Can use Apache Spark with airflow to schedule jobs to perform in PARELLEL
-
-"""
